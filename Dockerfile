0742ca0a2683ecd46b3109e36d16327d66c35757
Matt Schramm <schrummy14@gmail.com>
2023-08-02 14:31:57 -0500

    3.6.1 (#3)
    * Update to Blender 3.5.0
    
    * Blender Benchmark
    
    Added a benchmark option for testing GPU scaling.
    Currently only two images are available (Check the README.md).
    Options
    	Control if CPU is used
    	Set number of GPUs to be used
    	Turn off GPU allowing CPU only runs
    
    * Added author information for each image
    
    * Update to 3.6.1 with benchmarking
    
    ---------
    
    Co-authored-by: mschramm <matthewwschramm@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,25 @@
-FROM ubuntu:bionic
+FROM ubuntu:jammy
 LABEL maintainer="Nimbix, Inc."
 
 # Update SERIAL_NUMBER to force rebuild of all layers (don't use cached layers)
 ARG SERIAL_NUMBER
-ENV SERIAL_NUMBER ${SERIAL_NUMBER:-20200616.1800}
+ENV SERIAL_NUMBER ${SERIAL_NUMBER:-20230721.1000}
 
 RUN apt-get -y update && \
-    apt-get -y install curl ca-certificates && \
+    DEBIAN_FRONTEND=noninteractive apt-get -y install ca-certificates curl --no-install-recommends && \
     curl -H 'Cache-Control: no-cache' \
-        https://raw.githubusercontent.com/nimbix/image-common/master/install-nimbix.sh \
-        | bash -s -- --setup-nimbix-desktop
+        https://raw.githubusercontent.com/nimbix/jarvice-desktop/master/install-nimbix.sh \
+        | bash
 
 WORKDIR /opt/blender
 
 # Download from a mirror site
-RUN curl -o blender.tgz https://download.blender.org/release/Blender3.2/blender-3.2.0-linux-x64.tar.xz && \
+RUN curl -o blender.tgz https://mirrors.ocf.berkeley.edu/blender/release/Blender3.6/blender-3.6.1-linux-x64.tar.xz && \
     tar xf blender.tgz --strip-components=1 && \
     rm -f blender.tgz
 
 COPY scripts /usr/local/scripts
+COPY benchmark /opt/blender/benchmark
 
 COPY NAE/AppDef.json /etc/NAE/AppDef.json
 RUN curl --fail -X POST -d @/etc/NAE/AppDef.json https://cloud.nimbix.net/api/jarvice/validate
---
+++ benchmark/gpuSelection.py
@@ -0,0 +1,68 @@
+import bpy
+import sys
+
+# Copyright (c) 2023, Nimbix, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation are
+# those of the authors and should not be interpreted as representing official
+# policies, either expressed or implied, of Nimbix, Inc.
+
+def enable_gpus(numCpus, numGpus):
+
+    print("Running CPU/GPU Selection")
+
+    numCpus = int(numCpus)
+    numGpus = int(numGpus)
+
+    preferences = bpy.context.preferences
+    cycles_preferences = preferences.addons["cycles"].preferences
+    cycles_preferences.refresh_devices()
+    devices = cycles_preferences.devices
+    device_type = "CUDA"
+
+    if not devices:
+        raise RuntimeError("Unsupported device type")
+
+    activated_gpus = []
+    curNumGpu = 0
+    for device in devices:
+        if device.type == "CPU":
+            device.use = (numCpus > 0)
+        else:
+            if not (curNumGpu < numGpus):
+                continue
+            curNumGpu += 1
+            device.use = True
+            activated_gpus.append(device.name)
+        if device.use:
+            print(f'Activated {device.type}: {device.name}')
+
+    cycles_preferences.compute_device_type = device_type
+    bpy.context.scene.cycles.device = "GPU"
+
+    return activated_gpus
+
+
+enable_gpus(sys.argv[-2], sys.argv[-1])
